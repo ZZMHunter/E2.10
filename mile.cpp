a027ccbe58b03f812c2ad33d1012b5652725587f
ZZMHunter <zhao.zhang50@myhunter.cuny.edu>
2025-09-08 08:44:49 -0400

    finished E2.10

---
+++ mile.cpp
@@ -0,0 +1,26 @@
+/*
+    Author: Zhao Zhang
+    Course: CSCI-135
+    Instructor: Tong Yi
+    Assignment: E2.10
+
+    Input number of gallons of gas in the tank, fuel efficiency in miles per gallon, and price of gas per gallon
+    Then print the cost per 100 miles and how far the car can go with the gas in the tank
+*/
+
+#include <iostream>
+
+int main(){
+    int gallons;
+    int fuelEfficiency;
+    int pricePerGallon;
+    std::cout << "Enter gallons of gas in the tank: ";
+    std::cin >> gallons;
+    std::cout << "Enter fuel efficiency of car in miles per gallon: ";
+    std::cin >> fuelEfficiency;
+    std::cout << "Enter price of gas per gallon: ";
+    std::cin >> pricePerGallon;
+    std::cout << "The cost to drive the car 100 miles is $" << fuelEfficiency/100 * pricePerGallon << std::endl;
+    std::cout << "The car can drive " << gallons*fuelEfficiency << " miles with the amount of fuel in the tank" << std::endl;
+    return 0;
+}
